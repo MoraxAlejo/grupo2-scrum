04d87b18bf92c784ed57b6df31a0ddbe774b1ea5
Ccalos7319 <ccarlosvalencia73193563@gmail.com>
2023-09-28 11:08:38 -0500

    CROD evaluaciones

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EvaluacionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::controller(EvaluacionController::class)->group(function () {
+    Route::get('/evaluacion', 'index');
+    Route::get('/evaluacion/{id}', 'show');
+    Route::post('/evaluacion', 'store');
+    Route::put('/evaluacion/{id}', 'edit');
+    Route::delete('/evaluacion/{id}', 'destroy');
+   
 });
