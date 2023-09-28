d9fef86224d3e109d84f80a9f905286bf74378a9
jarvin <mangomc03@gmail.com>
2023-09-28 17:39:48 -0400

    instalando tailwind

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -3,15 +3,7 @@
 export default function Dashboard() {
   return (
     <div>
-        <form action="">
-            <h1>Login</h1>
-            <div>
-                <input type="text" placeholder="Email/Username"/>
-            </div>
-            <div>
-                <input type="text" placeholder="Password" />
-            </div>
-        </form>
+        
     </div>
   )
 }
